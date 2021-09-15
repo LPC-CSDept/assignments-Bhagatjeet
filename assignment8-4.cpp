36cdde81a06e29eee88ff06601810c7b1473b927
Bhagatjeet Dhillon <89234317+Bhagatjeet@users.noreply.github.com>
2021-09-15 02:38:12 +0000

    Added two for loops and copy pasted the void program from assignment 8-3

--- assignment8-4.cpp
+++ assignment8-4.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 
-int findMin(int [], int, int);
 void printArray(int [], int);
 
 int main()
@@ -10,4 +9,23 @@ int main()
 	int numbers[N] = {25, 10, 15, 30, 35, 40, 45, 55, 20, 50};
 
 
+	for ( N-1 times)
+		for ( all pairs)
+			if ( left > right)
+				swap ( );
+
+
+
+	printArray(numbers, N);
+
+
+}
+
+
+void printArray(int a[], int x)
+{
+	for(int i = 0; i < x; i++){
+    cout << a[i] << "\t";
+  }
+  cout << "\n";
 }
