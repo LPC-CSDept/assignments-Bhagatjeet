0d51f4238aceef291bf56d3af314578ca2c8f4f6
Bhagatjeet Dhillon <89234317+Bhagatjeet@users.noreply.github.com>
2021-09-15 02:23:54 +0000

    intial code

---
+++ assignment8-4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int findMin(int [], int, int);
+void printArray(int [], int);
+
+int main()
+{
+	const int N = 10;
+	int numbers[N] = {25, 10, 15, 30, 35, 40, 45, 55, 20, 50};
+
+
+}
