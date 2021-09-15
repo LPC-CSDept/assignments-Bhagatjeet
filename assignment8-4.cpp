a389db7a365fd38755d42c3ac01772a61fb15c0c
Bhagatjeet Dhillon <89234317+Bhagatjeet@users.noreply.github.com>
2021-09-15 02:44:24 +0000

    added specifics into the for loops and put the swapping code in place

--- assignment8-4.cpp
+++ assignment8-4.cpp
@@ -5,22 +5,19 @@ void printArray(int [], int);
 
 int main()
 {
+
 	const int N = 10;
 	int numbers[N] = {25, 10, 15, 30, 35, 40, 45, 55, 20, 50};
-
-
-	for ( N-1 times)
-		for ( all pairs)
-			if ( left > right)
-				swap ( );
-
-
-
+	
 	printArray(numbers, N);
 
+	for (int i=0; i<N-1; i++)
+		for(int j=0; j<N-1; j++)
+			if ( numbers[j] > numbers[j+1])
+				swap (numbers[j], numbers[j+1] );
 
+	printArray(numbers, N);
 }
-
 
 void printArray(int a[], int x)
 {
