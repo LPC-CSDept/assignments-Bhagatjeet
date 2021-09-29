e4fc7a98289819efa3daabb81589383a208a33a4
Bhagatjeet Dhillon <89234317+Bhagatjeet@users.noreply.github.com>
2021-09-29 04:26:41 +0000

    Set up the programming for both assignments and are submitting them

---
+++ a9-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double 	num1, num2, num3, num4, num5;
+	num1 = 10;
+	num2 = 20;
+	num3 = 30;
+	num4 = 40;
+	num5 = 50;
+
+	double 	* ptr;
+	ptr =  &num1;
+
+	cout << *ptr << endl; 
+	cout << *(ptr+1) << endl; 
+  cout << *(ptr+2) << endl;
+  cout << *(ptr+3) << endl;
+  cout << *(ptr+3) << endl;
+}
