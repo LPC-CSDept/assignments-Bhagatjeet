03b4765a7cfbe4e9fc588e734c3cb681a2160b0b
Bhagatjeet Dhillon <89234317+Bhagatjeet@users.noreply.github.com>
2021-10-19 22:55:03 +0000

    created all three files and finalized program for Question 1

---
+++ quiz2-1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+int  *fillupArray(int N);
+void printArray(int * const ptr, int N);
+
+int main()
+{
+    srand(time(0));
+    const int N=10;
+    int *ptr=nullptr;
+    ptr = fillupArray(N);
+    printArray(ptr, N);
+}
+int *fillupArray(int N)
+{
+    int *num = new int[N];
+    for (int i=0; i<N; i++){
+        num[i]=rand()% 100;
+    }
+    return num;
+}
+void printArray(int *const ptr, int N)
+{
+    for (int i=0; i<N; i++){
+        cout << ptr[i] << endl;
+    }
+}
