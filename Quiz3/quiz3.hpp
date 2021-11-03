2f88d0168a212b0236b3d7988f90fb6480cc7418
Bhagatjeet Dhillon <89234317+Bhagatjeet@users.noreply.github.com>
2021-11-03 01:33:24 +0000

    added class

---
+++ Quiz3/quiz3.hpp
@@ -0,0 +1,24 @@
+
+#ifndef Number_H
+#define Number_H
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class NUMBERS
+{
+  private:
+  int ID;
+  vector <int> numbers;
+  public:
+  int getID() const;
+  int getSize() const;
+  int getElm(int i) const;
+  int getMax() const ;
+  int getMin() const;
+  int getSum() const;
+  void deleteElm(int d) ;
+  void addElm(int a) ;
+  void printAll( ) const;
+}
