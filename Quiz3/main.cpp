a28bf79fb40cdb3fe533ce528fac1e1b07b7494b
Bhagatjeet Dhillon <89234317+Bhagatjeet@users.noreply.github.com>
2021-11-03 23:23:22 +0000

    copy code given for main

--- Quiz3/main.cpp
+++ Quiz3/main.cpp
@@ -7,7 +7,24 @@ int findMax(Numbers[], int);
 void deDup(Numbers &, Numbers &);
 int main() 
 {
-  
+  const int N = 5;
+  Numbers numberset[] = {Numbers(1, 5), Numbers(2, 5), Numbers(3, 10), Numbers(4, 5), Numbers(5, 7)};
+  for (int i = 0; i < N; i++) 
+  {
+    cout << "Number Set ID " << numberset[i].getID() << "\t\n";
+    numberset[i].printAll();
+    cout << "\n";
+  }
+  int id = findMax(numberset, N);
+  cout << "The set ID that has max difference is " << id << "\t";
+  cout << "The diff is " << numberset[id-1].getMax() - numberset[id-1].getMin() << endl;
+  deDup(numberset[2], numberset[3]);
+  cout << "ID 3 - ID 4\n";
+  numberset[2].printAll();
+  cout << "\n";
+  deDup(numberset[0], numberset[1]);
+  cout << "ID 1 - ID 2\n";
+  numberset[0].printAll();
 }
 
 int findMax(Numbers nums[], int size) 
