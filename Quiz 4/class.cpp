b5b1b3b0ed36129cd7fe06cb3fa9c5946a02a958
Bhagatjeet Dhillon <89234317+Bhagatjeet@users.noreply.github.com>
2021-11-24 03:01:24 +0000

    update

--- Quiz 4/class.cpp
+++ Quiz 4/class.cpp
@@ -13,13 +13,62 @@ Course::Course()
 {
   NUM_COURSES += 1;
 }
-
 Course::Course(string cname, int credits, string semester, vector <Student> student)
 {
   this->cname = cname;
   this->credits = credits;
   this->semester = semester;
   this->student = student;
-
   NUM_COURSES += 1;
+}
+string Course::getCName(void)
+{
+  return cname;
+}
+int Course::getCredits(void)
+{
+  return credits;
+}
+string Course::getSemester(void)
+{
+  return semester;
+}
+vector<Student> Course::getStudent(void)
+{
+  return student;
+}
+void Course::setCName(string cname)
+{
+  this->cname = cname;
+}
+void Course::setCredits(int credits)
+{
+  this->credits = credits;
+}
+void Course::setSemester(string semester)
+{
+  this->semester = semester;
+}
+void Course::setStudent(vector<Student> student)
+{
+  this->student = student;
+}
+void Course::addStudent(Student &s)
+{
+  this->student.push_back(s);
+}
+void Course::setCourse(string cname, int credits, string semester,  vector<Student> student)
+{
+  this->cname = cname;
+  this->credits = credits;
+  this->semester = semester;
+  this->student = student;
+}
+void Course::printCourse(void)
+{
+
+}
+int Course::getNumCourses(void)
+{
+  return NUM_COURSES;
 }
