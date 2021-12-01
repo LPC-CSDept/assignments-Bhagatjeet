336405fb9ec162225a53b09130d4fe0f95e99c9a
Bhagatjeet Dhillon <89234317+Bhagatjeet@users.noreply.github.com>
2021-12-01 04:37:50 +0000

    update

--- midterm2/student.hpp
+++ midterm2/student.hpp
@@ -28,3 +28,33 @@ void display(student a[], int n)
       if(!swaps)
       break;
     }
+    for(int i = 0; i < n; i++)
+    {
+      cout << "student id: " << a[i].id << "name: " << a[i].name<< " total score is: " << a[i].score1 + a[i].score2 + a[i].score3 << endl;
+    }
+  }
+
+  int binarysearch(student a[], int n, int id)
+  {
+    int start = 0;
+    int end = n-1;
+
+    while(start<=end)
+    {
+      int mid = (start+end)/2;
+
+      if(a[mid].id == id)
+      {
+        return mid;
+      }
+      else if(a[mid].id >id)
+      {
+        start = mid-1;
+      }
+      else{
+        end = mid - 1;
+      }
+    }
+    return -1;
+  }
+};
