5c673f1b947723e239d8eb84534f27f6df48f77c
Bhagatjeet Dhillon <89234317+Bhagatjeet@users.noreply.github.com>
2021-12-01 04:45:39 +0000

    update

---
+++ midterm2/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include "student.hpp"
+
+using namespace std;
+
+int main()
+{
+  int n = 10;
+  student a[10];
+
+  for(int i = 0; i < n; i++)
+  {
+    cout << "enter student id: ";
+    cin >> a[i].id;
+
+    cout << "enter student name: ";
+    cin >> a[i].name;
+
+    cout << "enter score 1: ";
+    cin >> a[i].score1;
+
+    cout << "enter score 2: ";
+    cin >> a[i].score2;
+
+    cout << "enter score 3: ";
+    cin >> a[i].score3;
+
+    a[i].total_scores = a[i].score1 + a[i].score2 + a[i].score3;
+  }
