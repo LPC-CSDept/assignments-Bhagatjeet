a20fdc85570df6c1c982e108406e1cadd667d27e
Bhagatjeet Dhillon <89234317+Bhagatjeet@users.noreply.github.com>
2021-12-01 03:37:19 +0000

    update

---
+++ midterm1/Numbers.hpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <vector>
+
+using namespace std;
+
+class Numbers
+{
+  private:
+  int ID;
+  vector<int>values;
+  static int numOfObjects;
+
+  public:
+  Numbers()
+  {
+    ID = 0;
+    numOfObjects++;
+  }
+
+  Numbers(int i, int n)
+  {
+    ID = i;
+    for(i = 0; i < n; i++)
+    {
+      int num = (rand() % (99 - 0 + 1)) + 0;
+      values.push_back(num);
+    }
+    numOfObjects++;
+  }
