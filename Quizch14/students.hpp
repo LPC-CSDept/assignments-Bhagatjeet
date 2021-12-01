5dfa1e3e2f344effd95d3b08ab8caa500b417538
Bhagatjeet Dhillon <89234317+Bhagatjeet@users.noreply.github.com>
2021-12-01 01:01:13 +0000

    update

--- Quizch14/students.hpp
+++ Quizch14/students.hpp
@@ -1,7 +1,6 @@
 #ifndef 	STUDENTS_H
 #define 	STUDENTS_H
 #include <string>
-
 using namespace std;
 
 class Student{
@@ -17,4 +16,47 @@ class Student{
       
       scores = 0;
     };
-    
+    Student(int id, string name, double score){
+      ID = id;
+      studentName = name;
+      scores = score;
+      if (score >= 90){
+        grade = 'A';
+      } else if (score >= 80) {
+        grade = 'B';
+      } else if (score >= 70) {
+        grade = 'C';
+      } else if (score >= 60) {
+        grade = 'D';
+      } else if (score < 60) {
+        grade = 'W';
+      } 
+    }
+    void setID(int id){
+      ID = id;
+    };
+    void setStudentName(string name){
+      studentName = name;
+    };
+    void setGrade(char GRADE){
+      grade = GRADE;
+    };
+    void setScores(double SCORE){
+      scores = SCORE;
+    };
+    int getID(){
+      return ID;
+    };
+    string getStudentName(){
+      return studentName;
+    };
+    char getGrade(){
+      return grade;
+    };
+    double getScores(){
+      return scores;
+    };
+
+    friend class Course;
+};
+#endif
