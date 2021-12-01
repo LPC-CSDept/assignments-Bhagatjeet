255ae49ffe9126f30fed97e143dc57ddfa15a81e
Bhagatjeet Dhillon <89234317+Bhagatjeet@users.noreply.github.com>
2021-12-01 04:30:24 +0000

    update

--- midterm2/student.hpp
+++ midterm2/student.hpp
@@ -11,3 +11,20 @@ class student
   int score2;
   int score3;
   int total_scores;
+void display(student a[], int n)
+  {
+    for(int i = 0; i < n; i++)
+    {
+      int swaps = 0;
+
+      for(int j = 0; j < n-i-1; j++)
+      {
+        if(a[j].total_scores < a[j+1].total_scores)
+        {
+          swap(a[j], a[j+1]);
+          swaps = 1;
+        }
+      }
+      if(!swaps)
+      break;
+    }
