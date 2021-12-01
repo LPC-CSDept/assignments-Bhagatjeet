f70d52c06e6511a9f59ece159662c8c9783375d4
Bhagatjeet Dhillon <89234317+Bhagatjeet@users.noreply.github.com>
2021-12-01 00:42:17 +0000

    update

---
+++ Quizch14/students.hpp
@@ -0,0 +1,20 @@
+#ifndef 	STUDENTS_H
+#define 	STUDENTS_H
+#include <string>
+
+using namespace std;
+
+class Student{
+  private:  
+    int ID;
+    string studentName;
+    char grade;
+    double scores;
+  public:
+    Student(){
+      ID = 0;
+      studentName = "No Data";
+      
+      scores = 0;
+    };
+    
