cf5c61f8d034cef5d12f4b40df06ee71f1673f2c
Bhagatjeet Dhillon <89234317+Bhagatjeet@users.noreply.github.com>
2021-12-14 00:49:05 +0000

    update and finish with .hpp

--- Final/final1/course.hpp
+++ Final/final1/course.hpp
@@ -13,7 +13,12 @@ class Course
 public:
 course(); // default constructor
   Course(int id, string name, int credit);// parameterized constructor
-
+int getId() const;// return course id
+  string &getName()const; // return course name
+  int getCredit() const;// return course credit
+  void setId(int id);//set course id
+  void setName(const string &name);//set course name
+  void setCredit(int credit); // set course credit
   void print() const;//display course details
-
 };
+#endif
