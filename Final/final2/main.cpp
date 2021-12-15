34729e3751049ee6bb2162a1623be6c5b90c84a2
Bhagatjeet Dhillon <89234317+Bhagatjeet@users.noreply.github.com>
2021-12-15 23:46:47 +0000

    starting final2 main.cpp

---
+++ Final/final2/main.cpp
@@ -0,0 +1,7 @@
+#include <fstream>
+#include "course.cpp"
+
+int partition (Course array[], int , int );
+void quickSort(Course array[], int, int );
+void swap(int* a, int* b);
+void displayArray(Course arr[], int size);
