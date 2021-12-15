d8d76e94992ce9043425a0b79b0d2b7132ab3941
Bhagatjeet Dhillon <89234317+Bhagatjeet@users.noreply.github.com>
2021-12-15 23:38:20 +0000

    made course.cpp for q2

---
+++ Final/final2/course.cpp
@@ -0,0 +1,40 @@
+#include "course.hpp"
+using namespace std;
+
+Course::Course()
+{
+  id = 0;
+  name = " ";
+  credit = 0;
+}
+
+Course::Course(int id, string name, int credit) : id(id), name(name), credit(credit){}
+
+int Course::getid() const
+{
+  return id;
+}
+ const string &Course::getname() const
+{
+  return name;
+}
+int Course::getcredit() const
+{
+  return credit;
+}
+void Course::setid(int id)
+{
+  this->id = id;
+}
+void Course::setname(const string &name)
+{
+  this->name = name;
+}
+void Course::setcredit(int credit)
+{
+  this->credit = credit;
+}
+void Course::print() const 
+{
+  cout << "Course{id =" << id << ", name = " << name << ", credit = "<< credit << "}" << endl;
+}
---
+++ Final/final2/course.hpp
@@ -0,0 +1,22 @@
+#ifndef COURSE_H
+#include <iostream>
+using namespace std;
+
+class Course
+{
+  private:
+  int id; // store the course id
+  string name; // store the course name
+  int credit; // store the course credit
+
+public:
+Course(); //default constructor
+Course(int id, string name, int credit);// parameterized constructor
+int getid() const;// return course id
+ const string &getname()const; // return course name
+  int getcredit() const;// return course credit
+  void setid(int id);//set course id
+  void setname(const string &name);//set course name
+  void setcredit(int credit); // set course credit
+  void print() const;//display course details
+};
