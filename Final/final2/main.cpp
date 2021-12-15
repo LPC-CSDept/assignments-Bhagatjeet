354bf8292ee36744f93edf08471fa09d56fdd31f
Bhagatjeet Dhillon <89234317+Bhagatjeet@users.noreply.github.com>
2021-12-15 23:58:38 +0000

    update

--- Final/final2/main.cpp
+++ Final/final2/main.cpp
@@ -5,3 +5,25 @@ int partition (Course array[], int , int );
 void quickSort(Course array[], int, int );
 void swap(int* a, int* b);
 void displayArray(Course arr[], int size);
+
+static int x;
+static string Inputfile;
+const int n = 10;
+
+int main()
+{
+
+}
+
+
+void operator>>(fstream& inputfile, Course& array){
+  int id;
+  string name;
+  int credit;
+  
+  inputfile.open(Inputfile, ios :: in);
+
+ 
+
+  inputfile.close();
+}
