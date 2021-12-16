090192475c2d7c97e143d5494b81d9411692a3ba
Bhagatjeet Dhillon <89234317+Bhagatjeet@users.noreply.github.com>
2021-12-16 23:24:40 +0000

    update

---
+++ Final/final3/employee.hpp
@@ -0,0 +1,14 @@
+#ifndef EMPLOYEE_H
+#include <iostream>
+using namespace std;
+
+class Employee{
+  protected:
+    string name;
+    int number;
+    string hiredate;
+  public:
+    employee();
+    
+};
+#endif
