eda0b69a0ec1c2a95a29d2f7a3faf6fa7495fee2
Bhagatjeet Dhillon <89234317+Bhagatjeet@users.noreply.github.com>
2021-12-17 00:46:57 +0000

    update

--- Final/final3/main.cpp
+++ Final/final3/main.cpp
@@ -10,9 +10,18 @@ ostream& operator<<(ostream& x, Employee& worker)
 }
  ostream& operator<<(ostream& x, Production& work)
  {
+  x << "Name: " << work.getname() << endl;
+  x << "ID: " << work.getid()<< endl;
+  x << "Hire Date: " << work.gethired()<< endl;
+  x << "Shift: " << work.getshift()<< endl;
+  x << "Pay: " << work.getpay()<< endl;
+  return x;
+ }
+ double operator > (const Production& y, const Production& z)
+ {
 
  }
-
+ 
 int main()
 {
   
